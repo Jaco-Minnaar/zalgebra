fcfcd8a16c6be16bf1643c038c9bd23eef50cc23
Jaco Minnaar <jminnaar@bscglobal.com>
2025-02-13 10:53:37 +0200

    update to zig 0.14

--- build.zig
+++ build.zig
@@ -23,9 +23,11 @@ pub fn build(b: *Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const main_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const run_main_tests = b.addRunArtifact(main_tests);
